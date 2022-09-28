ecbeeba4dced67e55425e3f3edeca52b410c022c
SMegalo <lang.jiang@ximalaya.com>
2022-09-28 19:27:00 +0800

    feat: 完成按钮组件的10%功能

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,23 @@
 module.exports = {
   ...require('gts/.prettierrc.json'),
 };
+
+// {
+//   "arrowParens": "always",
+//   "bracketSameLine": false,
+//   "bracketSpacing": true, // 带空格
+//   "embeddedLanguageFormatting": "auto",
+//   "htmlWhitespaceSensitivity": "css",
+//   "insertPragma": false,
+//   "jsxSingleQuote": false,
+//   "printWidth": 80,
+//   "proseWrap": "preserve",
+//   "quoteProps": "as-needed",
+//   "requirePragma": false,
+//   "semi": true,
+//   "singleQuote": false,
+//   "tabWidth": 2,
+//   "trailingComma": "es5",
+//   "useTabs": false,
+//   "vueIndentScriptAndStyle": false
+// }
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,15 +1,66 @@
-import {FC, forwardRef} from 'react';
+import {createRef, forwardRef} from 'react';
+import {tuple} from '../_utils/type';
+import type {SizeType} from '../cnfig-provider/SizeContext';
 import styles from './index.module.scss';
+import classNames from 'classnames';
 
-interface ButtonProps {
+const ButtonTypes = tuple(
+  'default',
+  'primary',
+  'ghost',
+  'dashed',
+  'link',
+  'text'
+);
+const ButtonHTMLTypes = tuple('submit', 'button', 'reset');
+
+export type ButtonType = typeof ButtonTypes[number];
+export type ButtonHTMLType = typeof ButtonHTMLTypes[number];
+
+export interface BaseButtonProps {
+  type?: ButtonType;
+  size?: SizeType;
+  disabled?: boolean;
+  loading?: boolean;
+  className?: string;
   children?: React.ReactNode;
 }
+
+// 区分标签按钮和普通按钮
+export type AnchorButtonProps = {
+  href: string;
+  target?: string;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+} & BaseButtonProps &
+  Omit<React.AnchorHTMLAttributes<never>, 'type' | 'onClick'>;
+
+export type NativeButtonProps = {
+  htmlType?: ButtonHTMLType;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+} & BaseButtonProps &
+  Omit<React.AnchorHTMLAttributes<never>, 'type' | 'onClick'>;
+
+export type ButtonProps = Partial<AnchorButtonProps & NativeButtonProps>;
 
 const BaseButton: React.ForwardRefRenderFunction<unknown, ButtonProps> = (
   props,
   ref
 ) => {
-  const buttonNode = <button {...props}></button>;
+  const {htmlType, disabled, className, ...otherProps} = props;
+
+  const buttonRef = (ref as any) || createRef<HTMLElement>();
+
+  const formatClassName = '';
+
+  const buttonNode = (
+    <button
+      {...(otherProps as NativeButtonProps)}
+      className={formatClassName}
+      type={htmlType}
+      disabled={disabled}
+      ref={buttonRef}
+    ></button>
+  );
 
   return buttonNode;
 };
